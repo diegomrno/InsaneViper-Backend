1181bdc1f5c69890589e0e6cda4242eaafe3f313
DiegoMoreno0640 <diegomoreno.pro45@gmail.com>
2022-03-11 10:52:01 +0000

    create : Main app paths

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-// To do
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    // To Do : redirect to home page
+})
+
+app.get('/home', (req, res) => {
+    // To Do : homepage (vitrine)
+})
+
+app.get('/app', (req, res) => {
+    // To Do : WebApp
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
